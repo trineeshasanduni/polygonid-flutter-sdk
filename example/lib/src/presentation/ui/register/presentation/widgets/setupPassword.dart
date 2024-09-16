855963cd2ce0bb29e0281943aa54fe35017e774a
Trineesha Thathsarani <86653839+trineeshasanduni@users.noreply.github.com>
2024-09-16 10:16:12 +0530

    update

--- example/lib/src/presentation/ui/register/presentation/widgets/setupPassword.dart
+++ example/lib/src/presentation/ui/register/presentation/widgets/setupPassword.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:video_player/video_player.dart';
